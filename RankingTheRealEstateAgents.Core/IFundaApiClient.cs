676d1f980da41cf2899e71e1017754e9ecec26a7
Melvin <mhzehl@gmail.com>
2019-05-23 20:27:35 +0200

    Add Core project with FundaClient and interface (responsible for setting up the connection) and implement HttpClientFactory

---
+++ RankingTheRealEstateAgents.Core/IFundaApiClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using RankingTheRealEstateAgents.Data.Models;
+
+namespace RankingTheRealEstateAgents.Core
+{
+    public interface IFundaApiClient
+    {
+        Task<FundaResponse> QueryAsync(string city, string filter, int page = 1);
+    }
+}
--- RankingTheRealEstateAgents.Web/Startup.cs
+++ RankingTheRealEstateAgents.Web/Startup.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using RankingTheRealEstateAgents.Core;
 
 namespace RankingTheRealEstateAgents.Web
 {
@@ -26,6 +21,12 @@ namespace RankingTheRealEstateAgents.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddHttpClient<IFundaApiClient, FundaApiClient>(client =>
+            {
+                var baseAddress = Configuration["FundaClient.BaseAddress"];
+
+                client.BaseAddress = new Uri(baseAddress);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
