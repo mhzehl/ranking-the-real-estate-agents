a38499e1b71034c7edf5badbbdb63cd162202e31
Melvin <mhzehl@gmail.com>
2019-05-23 20:15:20 +0200

    Add data project and create models

---
+++ RankingTheRealEstateAgents.Data/Models/RealEstateAgent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace RankingTheRealEstateAgents.Data.Models
+{
+    public class RealEstateAgent
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public List<RealEstateObject> RealEstateObjects { get; set; }
+        
+        public int ListingCount { get; set; }
+    }
+}
