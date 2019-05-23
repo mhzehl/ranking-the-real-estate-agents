d7461a1ec8bfb4d4603d056f2a384b2ce922fbf8
Melvin <mhzehl@gmail.com>
2019-05-23 23:56:11 +0200

    Add ranking and ranking table components. Change layout and add assets such as logo.

--- RankingTheRealEstateAgents.Web/clientapp/src/App.vue
+++ RankingTheRealEstateAgents.Web/clientapp/src/App.vue
@@ -2,32 +2,25 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Funda</span>
+        <span class="font-weight-light">Ranker</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <Ranking/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Ranking from './components/Ranking'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Ranking
   },
   data () {
     return {
@@ -36,3 +29,9 @@ export default {
   }
 }
 </script>
+
+<style>
+  .theme--light.v-toolbar {
+    background-color: #f7a100;
+  }
+</style>
