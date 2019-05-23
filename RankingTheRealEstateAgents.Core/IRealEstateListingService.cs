2b53fd5407bcca1902731ccb58aa19d6b8275e65
Melvin <mhzehl@gmail.com>
2019-05-23 22:09:18 +0200

    Add RealEstateListingService to process the Funda API response.

---
+++ RankingTheRealEstateAgents.Core/IRealEstateListingService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using RankingTheRealEstateAgents.Data.Models;
+
+namespace RankingTheRealEstateAgents.Core
+{
+    public interface IRealEstateListingService
+    {
+        Task<List<RealEstateObject>> GetRealEstateObjects(string city = "amsterdam", string filter = "tuin");
+    }
+}
