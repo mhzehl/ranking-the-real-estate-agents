0007fc4890ee9e580afc3ebfcd5cbd1e746f5533
Melvin <mhzehl@gmail.com>
2019-05-24 16:57:55 +0200

    Fix reference to Newtonsoft.Json

--- RankingTheRealEstateAgents.Core/ResilientFundaApiClient.cs
+++ RankingTheRealEstateAgents.Core/ResilientFundaApiClient.cs
@@ -1,6 +1,4 @@
-﻿using System.IO;
-using System.Net.Http;
-using System.Runtime.InteropServices.ComTypes;
+﻿using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
